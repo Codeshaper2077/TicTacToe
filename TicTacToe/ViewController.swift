e5ed12d6a3b8ea7b216b9fc9991484bd65a253db
Ivan <ivan.Nikolaevich55@gmail.com>
2024-01-23 02:02:19 +0500

    update: reset draw

--- TicTacToe/ViewController.swift
+++ TicTacToe/ViewController.swift
@@ -33,15 +33,66 @@ class ViewController: UIViewController {
     
     var NOUGHT = "O"
     var CROSS = "X"
+    var board = [UIButton]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        initBoard()
+    }
+    
+    func initBoard() {
+        board.append(a1)
+        board.append(a2)
+        board.append(a3)
+        board.append(b1)
+        board.append(b2)
+        board.append(b3)
+        board.append(c1)
+        board.append(c2)
+        board.append(c3)
+    }
+    
+    func fullBoard() -> Bool {
+        for button in board {
+            if button.title(for: .normal) == nil {
+                return false
+            }
+        }
+        return true
     }
     
     @IBAction func boardTapAction(_ sender: UIButton) {
         addToBoard(sender)
+        
+        if (fullBoard()) {
+            resultAlert(title: "Draw")
+        }
+        
     }
+    
+    func resultAlert(title: String) {
+        let ac = UIAlertController(title: title, message: nil, preferredStyle: .actionSheet)
+        ac.addAction(UIAlertAction(title: "Reset", style: .default, handler: { (_) in
+            self.resetBoard()
+        }))
+        self.present(ac, animated: true)
+    }
+    
+    func resetBoard() {
+        for button in board {
+            button.setTitle(nil, for: .normal)
+            button.isEnabled = true
+        }
+        if (firstTurn == Turn.Nought) {
+            firstTurn = Turn.Nought
+            turnLabel.text = CROSS
+        } else {
+            firstTurn = Turn.Nought
+            turnLabel.text = NOUGHT
+        }
+        currentTurn = firstTurn
+    }
+    
     func addToBoard(_ sender: UIButton) {
         if(sender.title(for: .normal) == nil) {
             if (currentTurn == Turn.Nought) {
@@ -53,6 +104,8 @@ class ViewController: UIViewController {
                 currentTurn = Turn.Nought
                 turnLabel.text = NOUGHT
             }
+            sender.setTitleColor(.black, for: .disabled)
+            sender.isEnabled = false
         }
     }
 }
